939d3bee6136e83e9885e35460412b0d59f95378
Jared Nolen <Legodoghead@gmail.com>
2021-06-11 12:46:39 -0700

    Added Basic Files

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: [],
+}
+
+//define game
+let game = new Phaser.Game(config);
