859c2790d5e0e71765095a42b08420b9c64c0d99
Daebreak II <krea@ucsc.edu>
2021-06-11 13:41:58 -0700

    added Tuts

--- src/prefabs/Tut.js
+++ src/prefabs/Tut.js
@@ -1,4 +1,4 @@
-class Player extends Phaser.Physics.Arcade.Sprite { 
+class Tut extends Phaser.Physics.Arcade.Sprite { 
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
     }
