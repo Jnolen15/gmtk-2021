9977767289b92d4dcf5b20fccf6ea7ab050a64e6
Daebreak II <krea@ucsc.edu>
2021-06-11 13:51:40 -0700

    added basic controls and added global variables

--- src/main.js
+++ src/main.js
@@ -10,3 +10,17 @@ let config = {
 
 //define game
 let game = new Phaser.Game(config);
+
+// defining keys
+// let keyF, KeyR, keyLEFT, keyRIGHT, keyUP, keyDOWN;
+keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
+// gameplay variables
+
+// Volume variables
+let volumeMultiplier = 1;
