471d611448ffcef9466062e23c67f3d557ccebf3
Daebreak II <krea@ucsc.edu>
2021-06-12 13:24:15 -0700

    added in animations to load, unused rn

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -9,7 +9,9 @@ class load extends Phaser.Scene {
         this.load.image('testPosMarkers', './assets/testAssets/blueSquare.png');
         this.load.image('testTut', './assets/testAssets/redSquare.png');
         this.load.atlas('tut', './assets/animations/anim_idle.png', './assets/animations/anim_idle.json');
+        this.load.atlas('tutWalk', './assets/animations/anim_tutwalk.png', './assets/animations/anim_tutwalk.json');
         this.load.atlas('leadTut', './assets/animations/anim_lead_idle.png', './assets/animations/anim_lead_idle.json');
+        this.load.atlas('leadTutWalk', './assets/animations/anim_lead_walk.png', './assets/animations/anim_lead_walk.json');
     }
 
     create(){
@@ -27,6 +29,18 @@ class load extends Phaser.Scene {
             frameRate: 8,
             repeat: -1
         });
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('tutWalk', {
+              start: 1,
+              end: 5,
+              zeroPad: 1,
+              prefix: 'hop',
+              suffix: '.png'
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
 
         this.anims.create({
             key: 'leadidle',
@@ -35,6 +49,18 @@ class load extends Phaser.Scene {
               end: 3,
               zeroPad: 1,
               prefix: 'leadidle',
+              suffix: '.png'
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'leadwalk',
+            frames: this.anims.generateFrameNames('leadTutWalk', {
+              start: 1,
+              end: 5,
+              zeroPad: 1,
+              prefix: 'leadhop',
               suffix: '.png'
             }),
             frameRate: 8,
